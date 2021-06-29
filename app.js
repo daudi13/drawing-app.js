df28cae21b2ec7bddaa486c3d48de63db2b1ec03
daudi13 <davouma13@gmail.com>
2021-06-29 09:29:16 +0300

    commit js

---
+++ app.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+
+let size = 20;
+let color = 'black'
+
+function drawCircle(x, y) {
+
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2, true);
+    ctx.fillStyle = color
+    ctx.fill()
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath()
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size;
+    ctx.stroke()
+}
+
+drawCircle(100, 200);
+drawLine(300, 300, 200, 500)
